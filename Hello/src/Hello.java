ada463230dcb5527877103b956724b616b6c3a4f
sgouthamraj <sgouthamraj@gmail.com>
2015-01-27 19:24:50 +0530

    step1

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -3,7 +3,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		System.out.print("New line added");
+
 		System.out.println("Hello World !!");
+
 	}
 
 }
