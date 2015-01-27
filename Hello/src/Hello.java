34034cf16bc5320232454c8085bdee74cc3161ef
mohanrajn <mohanmyworld@gmail.com>
2015-01-27 19:32:51 +0530

    - Resolved conflicts

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,10 +4,12 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello");
+		System.out.println("Hello F12 - New");
+
 		System.out.print("New line added");
 
 		System.out.println("Hello World 123!!");
+
 
 	}
 
