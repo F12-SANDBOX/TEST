14093ca6faa4cd9bc39a183547a6aaa691887834
mohanrajn <mohanmyworld@gmail.com>
2015-01-27 19:40:58 +0530

    Cool

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -4,8 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello F12 - New");
-
+		System.out.println("Hello F12");
 
 
 	}
